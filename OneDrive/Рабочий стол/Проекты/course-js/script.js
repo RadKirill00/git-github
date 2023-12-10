90375bb955d9a53cceeacbc92859ed5a17ff82da
Kirill <radkirill00@mail.ru>
2023-12-11 02:58:13 +0300

    frfr

--- OneDrive/Рабочий стол/Проекты/course-js/script.js
+++ OneDrive/Рабочий стол/Проекты/course-js/script.js
@@ -1,2 +1,9 @@
 'use strict'
-
+let arr = [[1, 2, 3, 4, 5], [6, 7, 8], [9, 10]];
+let sum = 0
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    sum += arr[i][j]
+  }
+}
+console.log(arr.flat());
