4fa0ce409058b6eb5803a6f23e351926439d43b9
Kirill <radkirill00@mail.ru>
2023-12-12 12:47:57 +0300

    fwef

--- OneDrive/Рабочий стол/Проекты/course-js/script.js
+++ OneDrive/Рабочий стол/Проекты/course-js/script.js
@@ -1,10 +1,3 @@
 'use strict'
-let arr = [[1, 2, 3, 4, 5], [6, 7, 8], [9, 10]];
-let sum = 0
-let arr2 = arr
-for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr[i].length; j++) {
-    sum += arr[i][j]
-  }
-}
-console.log(arr);
+let obj = {a: 1, b: 2, c: 3};
+console.log(Object.keys(obj));
