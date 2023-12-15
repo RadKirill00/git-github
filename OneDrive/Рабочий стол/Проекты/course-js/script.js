396dbf82a762d38c1095fcb99190a18755353dac
Kirill <radkirill00@mail.ru>
2023-12-15 16:16:44 +0300

    fefwe

--- OneDrive/Рабочий стол/Проекты/course-js/script.js
+++ OneDrive/Рабочий стол/Проекты/course-js/script.js
@@ -1,6 +1,11 @@
 'use strict'
-let obj = {
-	square: function(arr) {return arr * arr},
-	cube: function(arr) {return arr * arr * arr},
-};
-console.log(obj.square([1, 2, 3, 4]));
+
+function test(num, func1, func2) {
+    return func1(num) + func2(num);
+}
+
+const square = (x) => Math.pow(x, 2) 
+const double = (x) => Math.pow(x, 3)
+const res = test(4, square, double)
+console.log(res);
+
