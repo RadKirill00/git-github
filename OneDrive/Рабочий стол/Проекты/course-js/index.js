c67e5219e3906e41216713a3eeee49d9b813c817
Kirill <radkirill00@mail.ru>
2023-12-20 05:59:28 +0300

    dwedwe

---
+++ OneDrive/Рабочий стол/Проекты/course-js/index.js
@@ -0,0 +1,5 @@
+let result = (function() {
+	return '!';
+}());
+
+console.log(result);
--- OneDrive/Рабочий стол/Проекты/course-js/script.js
+++ OneDrive/Рабочий стол/Проекты/course-js/script.js
@@ -1,11 +1,1 @@
-'use strict'
-
-function test(num, func1, func2) {
-    return func1(num) + func2(num);
-}
-
-const square = (x) => Math.pow(x, 2) 
-const double = (x) => Math.pow(x, 3)
-const res = test(4, square, double)
-console.log(res);
 
