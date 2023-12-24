3225fa0e2cf49fccff8f62cc0289b7d5908eb507
Kirill <radkirill00@mail.ru>
2023-12-25 01:27:29 +0300

    DOM js update

--- OneDrive/Рабочий стол/Проекты/course-js/index.js
+++ OneDrive/Рабочий стол/Проекты/course-js/index.js
@@ -1,7 +1,11 @@
-let elem = document.querySelector('#qwe')
-let button = document.querySelector('#eee')
+let img1 = document.querySelector('#img')
+let qwe = document.querySelector('#qwe')
+let qwe2 = document.querySelector('#qwe2')
 
-button.addEventListener('click', () => {
-    elem.textContent = Number(elem.textContent) + 1
-    console.log(elem.textContent);
+qwe.addEventListener('click', () => {
+  img1.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHUFm4mqB_8kNeNXsZCn5MiwRU8YycF2k8Lg&usqp=CAU'
 })
+qwe2.addEventListener('click', () => {
+  img1.src = 'https://goo.su/logos/logo_blue_white.png'
+})
+
