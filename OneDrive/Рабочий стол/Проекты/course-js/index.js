7446af5fe57b45b907756a02f2a7856d969eceb9
Kirill <radkirill00@mail.ru>
2023-12-28 09:09:41 +0300

    fwf

--- OneDrive/Рабочий стол/Проекты/course-js/index.js
+++ OneDrive/Рабочий стол/Проекты/course-js/index.js
@@ -1,11 +1,16 @@
-let img1 = document.querySelector('#img')
-let qwe = document.querySelector('#qwe')
-let qwe2 = document.querySelector('#qwe2')
 
-qwe.addEventListener('click', () => {
-  img1.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHUFm4mqB_8kNeNXsZCn5MiwRU8YycF2k8Lg&usqp=CAU'
-})
-qwe2.addEventListener('click', () => {
-  img1.src = 'https://goo.su/logos/logo_blue_white.png'
-})
+let p = document.querySelectorAll('.qwe');
+for (let elem of p) {
+  elem.addEventListener('click', func)
+}
+function func() {
+  this.textContent = this.textContent + '!'
+  this.removeEventListener('click', func)
+}
+
+
+
+
+
+
 
