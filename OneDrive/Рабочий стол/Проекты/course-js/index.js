1f20e8436fa3eb8d13bb40cbb3b227e6ea5c8514
Kirill <radkirill00@mail.ru>
2023-12-28 18:23:47 +0300

    reduce

--- OneDrive/Рабочий стол/Проекты/course-js/index.js
+++ OneDrive/Рабочий стол/Проекты/course-js/index.js
@@ -1,12 +1,17 @@
+let elem = document.querySelectorAll('li');
+let res = document.querySelector('p');
+let but = document.querySelector('button');
+let sum = 0
+for (let elems of elem) {
+  if (elems.textContent.toString().split('').reduce((a, b) => +a + +b) == 6) {
+    sum += +elems.textContent;
+  }
+}
+but.addEventListener('click', () => {
+  res.textContent = sum;
+})
 
-let p = document.querySelectorAll('.qwe');
-for (let elem of p) {
-  elem.addEventListener('click', func)
-}
-function func() {
-  this.textContent = this.textContent + '!'
-  this.removeEventListener('click', func)
-}
+
 
 
 
