747014fd1b86d840df268790ca856b7af2e97e7b
Kirill <radkirill00@mail.ru>
2023-12-31 20:46:36 +0300

    update react

--- react-qwe/src/Practice.jsx
+++ react-qwe/src/Practice.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
 
-  const Practice = () => {
-    const isAuth = true;
-	
-    return <div>
-      {isAuth && <p>Bы авторизованы</p>}
-      {!isAuth && <p>Please signUp</p>}
-    </div>;
-  
+
+// Давайте положим каждый элемент этого массива в абзац и выведем эти абзацы в диве. Для этого мы можем воспользоваться любым удобным циклом JavaScript. Например, обычным for-of: 
+// Однако, в React для таких дел более принято использовать метод map:
+
+//   const arr = ['a', 'b', 'c', 'd', 'e'];
+//   const res = arr.map(function(num){
+//     return <li>{num}</li>
+//   })
+//   return <ul>
+//     {res}
+//   </ul>
+
+  function Practice() {
+    const users = [
+      { id: 1, name: 'user1', surn: 'surn1', age: 30},
+      { id: 2, name: 'user2', surn: 'surn2', age: 31},
+      { id: 3, name: 'user3', surn: 'surn3', age: 32},
+    ];
+
+    const rows = users.map(function(item) {
+      return <tr key={item.id}>
+        <td>{item.name}</td>
+        <td>{item.age}</td>
+      </tr>;
+    });
+    
+    return <table>
+      <thead>
+        <tr>
+          <td>название</td>
+          <td>стоимость</td>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>;
 }
 export default Practice
+
+
+
+
