d237d003e6aa68b6d73c6fb2106f386824576ff8
Kirill <radkirill00@mail.ru>
2024-01-05 01:07:11 +0300

    React update

--- react-qwe/src/Practice.jsx
+++ react-qwe/src/Practice.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { nanoid } from 'nanoid'
+import { useState } from 'react';
 
 // Давайте положим каждый элемент этого массива в абзац и выведем эти абзацы в диве. Для этого мы можем воспользоваться любым удобным циклом JavaScript. Например, обычным for-of: 
 // Однако, в React для таких дел более принято использовать метод map:
@@ -11,15 +12,26 @@ import { nanoid } from 'nanoid'
 //   return <ul>
 //     {res}
 //   </ul>
-  let res
-  function Practice() {
+  function Practice() { 
+    const [res, setRes] = useState(1)
+    function changeHandler(event) {
+      setRes(event.target.value);
+    }
+    let num1 = 'HTML'
+    let num2 = 'CSS'
+    let num3 = 'JS'
 
-  
-}
+    return <div className="">
+      <p> 
+      {num1}  <input type="radio" name='radio' value={1} checked={res === '1' ? true : false} onChange={changeHandler}/>
+      {num2}  <input type="radio" name='radio' value={2} checked={res === '2' ? true : false} onChange={changeHandler}/>
+      {num3}  <input type="radio" name='radio' value={3} checked={res === '3' ? true : false} onChange={changeHandler}/></p>
+      <p>{res === '1' ? num1 : null}
+        {res === '2' ? num2 : null}
+        {res ==='3' ? 'Вы молодец, JS очень хороший выбор' : null}
 
-// Это функция, которая использует библиотеку 'nanoid()' для генерации случайных чисел для айди 
-function id() {
-  return nanoid()
+      </p>
+    </div>
 }
 
 
