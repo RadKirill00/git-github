3c15522e97f59eae48facbd40e2c4271f4785b67
Kirill <radkirill00@mail.ru>
2024-01-16 22:22:33 +0300

    qwe

--- react-qwe/src/Practice.jsx
+++ react-qwe/src/Practice.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 
 const Practice = () => {
+
+  let sum = 0
+
+  function getDigitsSum(num) {
+    num.toString().split('').forEach(digit => sum += Number(digit));
+  }
   
+  getDigitsSum(1233)
+
+  return (
+    <div>
+      {sum}    
+    </div>
+  )
+    
 };
 
 export default Practice;
