dc1087283ebc67390a08b0a8aed87e36c77df0ab
Kirill <radkirill00@mail.ru>
2024-01-20 07:24:55 +0300

    Made a background change

--- react-qwe/src/Practice.jsx
+++ react-qwe/src/Practice.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import qwe from "./qwe";
 import Card from "./Card";
 
 const Practice = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleThemeChange = () => {
+    setIsDarkMode(!isDarkMode);
+
+    document.body.style.backgroundColor = isDarkMode ? "#1C1C1C" : "#000000";
+  };
+
+  const themeButtonStyle = {
+    width: "50px",
+    height: "50px",
+    borderRadius: "50px",
+    color: "#6B6B6B",
+    border: "1px solid #6B6B6B",
+    background: isDarkMode ? "#1C1C1C" : "black", // Change button color based on dark mode
+  };
+
   return (
     <>
       <div className="container">
@@ -14,6 +31,7 @@ const Practice = () => {
         <div className="myButtons">
           <button className="pause">| |</button>
           <button className="desc">...</button>
+          <button className="theme"  style={themeButtonStyle} onClick={handleThemeChange}></button>
         </div>
       </div>
       <div className="cards">
