d8afd0789dc4a1b5d5d6419650d5a1b4671a29ee
Kirill <radkirill00@mail.ru>
2024-01-28 01:43:48 +0300

    update

--- react-qwe/src/App.js
+++ react-qwe/src/App.js
@@ -3,15 +3,28 @@ import Product from "./components/Product";
 import User from "./components/User";
 import Employee from "./components/Employee";
 import qwe from "./qwe";
+import styled from "styled-components";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 150px;
+`;
 
-
+const Input = styled.input`
+  margin: 5px;
+  font-size: 18px;
+`;
 function App() {
 
 
   return (
     <div>
-      
+      <Container>
+        <Input disabled/>
+        <Input />
+        <Input type="reset"/>
+      </Container>
       <Product />
       <div>
         {qwe.map((i) => (
