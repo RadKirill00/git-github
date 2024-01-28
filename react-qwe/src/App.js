074c1d13a0c6dc183f5d38004b5b1b9cd9d18275
Kirill <radkirill00@mail.ru>
2024-01-28 23:10:21 +0300

    pizdec

--- react-qwe/src/App.js
+++ react-qwe/src/App.js
@@ -4,21 +4,44 @@ import User from "./components/User";
 import Employee from "./components/Employee";
 import qwe from "./qwe";
 import styled from "styled-components";
-import classes from "./App.module.css"; 
+import classes from "./App.module.css";
 import Buttons from "./Buttons";
-
+import { useEffect } from "react";
 
 
 function App() {
+  const [blockVisible, setBlockVisible] = useState(false);
+  
+  function Visible() {
+    setBlockVisible(true);
+  }
+  function UnVisible() {
+    setBlockVisible(false);
+  }
 
+  function instr() {
+    
+    if (blockVisible) {
+      UnVisible();
+    }
+  }
 
+  useEffect(() => {
+    window.addEventListener("click", instr);
+
+    return () => {
+      window.removeEventListener("click", instr);
+    };
+  }, [blockVisible]);
   return (
-    <div class={classes.app}>
-      <h2 class={classes.title}>CSS Module Buttons</h2>
-      <div class={classes.wrapper}>
-        <Buttons />
-      </div>
+    <div>
+      <button onClick={(e) => { e.stopPropagation(); Visible(); }}>Показать блок</button>
+
+      {blockVisible && (
+        <div style={{ border: "1px solid black" }}>Блок отображается</div>
+      )}
     </div>
   );
 }
+
 export default App;
